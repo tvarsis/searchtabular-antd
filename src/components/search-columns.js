36c94758a5af364a16977fa48c42c1691f8aaa7d
vijayst <vijayst@gmail.com>
2017-05-14 21:43:43 +0530

    added checkbox

--- src/components/search-columns.js
+++ src/components/search-columns.js
@@ -2,38 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SearchColumns = ({ columns, query, onChange }) => {
-  const onQueryChange = (event) => {
-    onChange({
-      ...query,
-      [event.target.name]: event.target.value
-    });
-  };
+    const onQueryChange = (event) => {
+        onChange({
+            ...query,
+            [event.target.name]: event.target.value
+        });
+    };
 
-  return (
-    <tr>
-      {columns.map((column, i) => (
-        <th key={`${column.property || i}-column-filter`} className="column-filter">
-          {column && column.property ?
-            <input
-              onChange={onQueryChange}
-              className="column-filter-input"
-              name={column.property}
-              placeholder={column.filterPlaceholder || ''}
-              value={query[column.property] || ''}
-            />
-          : ''}
-        </th>
-      ))}
-    </tr>
-  );
-};
-SearchColumns.propTypes = {
-  columns: PropTypes.arrayOf(PropTypes.object).isRequired,
-  onChange: PropTypes.func.isRequired,
-  query: PropTypes.object
-};
-SearchColumns.defaultProps = {
-  query: {}
-};
+    return (
+        <tr>
+            {columns.map((column, i) => (
+                <th key={`${column.property || i}-column-filter`} className="column-filter">
+                    {column && column.property ?
+                        column.checkbox ?
+                        <input
+                            type="checkbox"
+                            className="column-filter-input"
+                            name={column.property}
+                        /> :
+                        <input
+                            onChange={onQueryChange}
+                            className="column-filter-input"
+                            name={column.property}
+                            placeholder={column.filterPlaceholder || ''}
+                            value={query[column.property] || ''}
+                        /> :
+                        ''}
+                    </th>
+                ))}
+            </tr>
+        );
+    };
+    SearchColumns.propTypes = {
+        columns: PropTypes.arrayOf(PropTypes.object).isRequired,
+        onChange: PropTypes.func.isRequired,
+        query: PropTypes.object
+    };
+    SearchColumns.defaultProps = {
+        query: {}
+    };
 
-export default SearchColumns;
+    export default SearchColumns;
