67deb752e03ef10706e07671d4388d22f03d3b09
Gustav <tvarsis@hotmail.com>
2025-07-19 13:10:46 +0200

    Update dist

--- dist/components/search-columns.js
+++ dist/components/search-columns.js
@@ -1,10 +1,11 @@
 "use strict";
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 var _datePicker = _interopRequireDefault(require("antd/lib/date-picker"));
 var _input = _interopRequireDefault(require("antd/lib/input"));
@@ -14,12 +15,18 @@ var _tooltip = _interopRequireDefault(require("antd/lib/tooltip"));
 var _reactIntlUniversal = _interopRequireDefault(require("react-intl-universal"));
 var _moment = _interopRequireDefault(require("rc-picker/lib/generate/moment"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _interopRequireWildcard(e, t) { if ("function" == typeof WeakMap) var r = new WeakMap(), n = new WeakMap(); return (_interopRequireWildcard = function _interopRequireWildcard(e, t) { if (!t && e && e.__esModule) return e; var o, i, f = { __proto__: null, "default": e }; if (null === e || "object" != _typeof(e) && "function" != typeof e) return f; if (o = t ? n : r) { if (o.has(e)) return o.get(e); o.set(e, f); } for (var _t in e) "default" !== _t && {}.hasOwnProperty.call(e, _t) && ((i = (o = Object.defineProperty) && Object.getOwnPropertyDescriptor(e, _t)) && (i.get || i.set) ? o(f, _t, i) : f[_t] = e[_t]); return f; })(e, t); }
 function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
 function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
 var AntDatePicker = _datePicker["default"].generatePicker(_moment["default"]);
 var Option = _select["default"].Option;
 var NUMBER_MAX = 2147483647 - 47;
@@ -132,30 +139,83 @@ function renderNumber(column, query, onMinNumberChange, onMaxNumberChange) {
     "aria-label": ((_column$header4 = column.header) === null || _column$header4 === void 0 ? void 0 : _column$header4.label) || column.property
   }))) : "";
 }
-function renderText(column, query, onQueryChange, tooltipTitle, shouldOpenTooltip) {
+function renderText(column, query, onQueryChange, tooltipTitle, shouldOpenTooltip, tooltipWikiLink) {
+  return column && column.property && !column.checkbox && column.type !== "reactElement" && column.type !== "date" && column.type !== "number" && column.type !== "dropdown" ? /*#__PURE__*/_react["default"].createElement(TextFilterWithTooltip, {
+    column: column,
+    query: query,
+    onQueryChange: onQueryChange,
+    tooltipTitle: tooltipTitle,
+    shouldOpenTooltip: shouldOpenTooltip,
+    tooltipWikiLink: tooltipWikiLink
+  }) : "";
+}
+var TextFilterWithTooltip = function TextFilterWithTooltip(_ref) {
   var _column$header5;
-  var showTooltip = shouldOpenTooltip(query[column.property]);
-  return column && column.property && !column.checkbox && column.type !== "reactElement" && column.type !== "date" && column.type !== "number" && column.type !== "dropdown" ? /*#__PURE__*/_react["default"].createElement(_tooltip["default"], {
-    title: tooltipTitle,
-    open: showTooltip
+  var column = _ref.column,
+    query = _ref.query,
+    onQueryChange = _ref.onQueryChange,
+    tooltipTitle = _ref.tooltipTitle,
+    shouldOpenTooltip = _ref.shouldOpenTooltip,
+    tooltipWikiLink = _ref.tooltipWikiLink;
+  var _useState = (0, _react.useState)(false),
+    _useState2 = _slicedToArray(_useState, 2),
+    tooltipVisible = _useState2[0],
+    setTooltipVisible = _useState2[1];
+  var timerIdRef = (0, _react.useRef)(null);
+  var inputValue = query[column.property] || "";
+  (0, _react.useEffect)(function () {
+    clearTimeout(timerIdRef.current);
+    if (shouldOpenTooltip(inputValue)) {
+      setTooltipVisible(true);
+      timerIdRef.current = setTimeout(function () {
+        setTooltipVisible(false);
+      }, 5000);
+    } else {
+      setTooltipVisible(false);
+    }
+    return function () {
+      clearTimeout(timerIdRef.current);
+    };
+  }, [inputValue, shouldOpenTooltip]);
+  var handleMouseEnter = function handleMouseEnter() {
+    // When the mouse enters the tooltip content, clear the hide timer and keep the tooltip visible
+    clearTimeout(timerIdRef.current);
+  };
+  var handleMouseLeave = function handleMouseLeave() {
+    timerIdRef.current = setTimeout(function () {
+      setTooltipVisible(false);
+    }, 5000);
+  };
+  var tooltipContent = /*#__PURE__*/_react["default"].createElement("div", {
+    onMouseEnter: handleMouseEnter,
+    onMouseLeave: handleMouseLeave
+  }, tooltipTitle, tooltipWikiLink && /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("a", {
+    href: tooltipWikiLink,
+    target: "_blank",
+    rel: "noopener noreferrer"
+  }, _reactIntlUniversal["default"].get("shared.clickForDetails"))));
+  return /*#__PURE__*/_react["default"].createElement(_tooltip["default"], {
+    title: tooltipContent,
+    open: tooltipVisible
   }, /*#__PURE__*/_react["default"].createElement(_input["default"], {
     onChange: onQueryChange,
     name: column.property,
     placeholder: column.filterPlaceholder || "",
-    value: query[column.property] || "",
+    value: inputValue,
     maxLength: TEXT_MAX_LENGTH,
     "aria-label": ((_column$header5 = column.header) === null || _column$header5 === void 0 ? void 0 : _column$header5.label) || column.property
-  })) : "";
-}
+  }));
+};
 function renderReactElement(column) {
   return column && column.property && column.type === "reactElement" ? /*#__PURE__*/_react["default"].createElement("div", null, column.reactElement) : "";
 }
-var SearchColumns = function SearchColumns(_ref) {
-  var columns = _ref.columns,
-    query = _ref.query,
-    onChange = _ref.onChange,
-    tooltipTitle = _ref.tooltipTitle,
-    shouldOpenTooltip = _ref.shouldOpenTooltip;
+var SearchColumns = function SearchColumns(_ref2) {
+  var columns = _ref2.columns,
+    query = _ref2.query,
+    onChange = _ref2.onChange,
+    tooltipTitle = _ref2.tooltipTitle,
+    shouldOpenTooltip = _ref2.shouldOpenTooltip,
+    tooltipWikiLink = _ref2.tooltipWikiLink;
   var onQueryChange = function onQueryChange(event) {
     onChange(_objectSpread(_objectSpread({}, query), {}, _defineProperty({}, event.target.name, event.target.value)));
   };
@@ -212,7 +272,7 @@ var SearchColumns = function SearchColumns(_ref) {
     return /*#__PURE__*/_react["default"].createElement("th", {
       key: "".concat(column.property || i, "-column-filter"),
       className: "column-filter"
-    }, renderReactElement(column), renderCheckbox(column, query, onCheckChange), renderDate(column, query, onMinDateChange, onMaxDateChange), renderNumber(column, query, onMinNumberChange, onMaxNumberChange), renderText(column, query, onQueryChange, tooltipTitle, shouldOpenTooltip), renderDropDown(column, query, onDropDownChange));
+    }, renderReactElement(column), renderCheckbox(column, query, onCheckChange), renderDate(column, query, onMinDateChange, onMaxDateChange), renderNumber(column, query, onMinNumberChange, onMaxNumberChange), renderText(column, query, onQueryChange, tooltipTitle, shouldOpenTooltip, tooltipWikiLink), renderDropDown(column, query, onDropDownChange));
   }));
 };
 SearchColumns.propTypes = {
@@ -220,12 +280,14 @@ SearchColumns.propTypes = {
   onChange: _propTypes["default"].func.isRequired,
   query: _propTypes["default"].object,
   tooltipTitle: _propTypes["default"].string,
-  shouldOpenTooltip: _propTypes["default"].func
+  shouldOpenTooltip: _propTypes["default"].func,
+  tooltipWikiLink: _propTypes["default"].string
 };
 SearchColumns.defaultProps = {
   query: {},
   shouldOpenTooltip: function shouldOpenTooltip() {
     return false;
-  }
+  },
+  tooltipWikiLink: "#"
 };
 var _default = exports["default"] = SearchColumns;
